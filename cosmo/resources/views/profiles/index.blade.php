f7cdaefe3140a752e4957ee3f96b45b569e6caaa
edbert19 <ed79bert@gmail.com>
2025-06-16 21:59:34 +0700

    commit 9 edbert: revisi postcontroller,show.blade.php, dan index.blade.php

--- cosmo/resources/views/profiles/index.blade.php
+++ cosmo/resources/views/profiles/index.blade.php
@@ -50,7 +50,7 @@
 
     <div class="row mt-5 row-cols-1 row-cols-sm-2 row-cols-md-3 g-4">
         @foreach($user->posts as $post)
-            <div class="col">
+            <div class="col position-relative">
                 <a href="{{ route('posts.show', $post->id) }}">
                     <div class="post-thumbnail-wrapper">
                         <img src="{{ asset('storage/' . $post->image_path) }}"
